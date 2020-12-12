5f45ba54e03c6b2e918e8efa4a8de43b2197d0cf
xAdlerx <xadlerx.developer@gmail.com>
2020-12-12 18:15:30 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,3 +6,8 @@ class MyClient(discord.Client):
         print("Ich habe mich eingeloggt")
         
     #Wenn meine Nachricht gepostet wird 
+    async def on_message(self, message):
+        print ("Nachricht von " + str(message.author) + " enthält " + str(message.content)
+              
+ client = MyClient()
+ client.run("0KX_oE1i9T_GQe_m8CKh88OTImlU3PuR")
